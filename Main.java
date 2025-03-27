db68c75019b90b3c319d00f27de4c667555fb516
awrteries <levic0rrpus@gmail.com>
2025-03-27 03:01:35 -0500

    thank you lord!

--- Main.java
+++ Main.java
@@ -1,3 +1,32 @@
-public class Main {
+import java.awt.*;
+import javax.swing.*;
+
+public class Main extends JFrame{
+	private static final int WIDTH =1800;
+	private static final int HEIGHT=1600;
+	
+	public Main () {
+		super("conf");
+		setSize(WIDTH, HEIGHT);
+		Game play = new Game();
+		((Component) play).setFocusable(true);
+
+		
+		setBackground(Color.white);
+		
+		getContentPane().add(play);
+		
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+
+	public static void main(String[] args) {
+		Main run = new Main();
+		
+
+	}
+
 
 }
