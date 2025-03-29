63e6d0e18223ddc041333d9b102d9677fe9ca702
awrteries <levic0rrpus@gmail.com>
2025-03-29 16:52:35 -0500

    added fern idles

--- Fern.java
+++ Fern.java
@@ -7,7 +7,7 @@ public class Fern extends Npcs {
 
     public Fern(int x, int y){
         super("Fern", x, y, 0, 0, 59, 100, 
-        " ", "assets/baseidlel.gif", "assets/baseidler.gif", "assets/basewalkd.gif", "assets/basewalku.gif", "assets/basewalkl.gif", "assets/basewalkr.gif",
+        " ", "assets/npcs/fox/foxidlel.gif", "assets/npcs/fox/foxidler.gif", "assets/basewalkd.gif", "assets/basewalku.gif", "assets/basewalkl.gif", "assets/basewalkr.gif",
         false);
         super.setSprite(getIdleL());
     }
