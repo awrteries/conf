807f62263da65758acad0bdc5943f0688a7f4023
awrteries <levic0rrpus@gmail.com>
2025-03-31 16:57:08 -0500

    updated a bunch of sprites

--- Lien.java
+++ Lien.java
@@ -7,7 +7,7 @@ public class Lien extends Npcs{
 
     public Lien(int x, int y){
         super("Liên", x, y, 0, 0, 59, 100, 
-        "assets/player/baseidler.gif", "assets/player/baseidlel.gif", "assets/player/baseidler.gif", "assets/basewalkd.gif", "assets/basewalku.gif", "assets/basewalkl.gif", "assets/basewalkr.gif",
+        "assets/npcs/penguin/pengidler.gif", "assets/npcs/penguin/pengidlel.gif", "assets/npcs/penguin/pengidler.gif", "assets/basewalkd.gif", "assets/basewalku.gif", "assets/basewalkl.gif", "assets/basewalkr.gif",
         false,
         new Dialogue("peng's dialogue", "assets/dialogue/pengDialogue"));
     }
--- Valentino.java
+++ Valentino.java
@@ -7,7 +7,7 @@ public class Valentino extends Npcs{
     }
 
     public Valentino(int x, int y){
-        super("Valentino", x, y, 0, 0, 59, 100, 
+        super("Valentino", x, y, 0, 0, 61, 100, 
         "assets/npcs/vampire/validler.gif", "assets/npcs/vampire/validlel.gif", "assets/npcs/vampire/validler.gif", "assets/basewalkd.gif", "assets/basewalku.gif", "assets/basewalkl.gif", "assets/basewalkr.gif",
         false,
         new Dialogue("valentino's dialogue", "assets/dialogue/valDialogue"));
