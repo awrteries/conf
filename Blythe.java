981a7e27f4a26456e0474464a2ed6f8e5e316ca5
awrteries <levic0rrpus@gmail.com>
2025-04-02 23:58:31 -0500

    blimey

--- Blythe.java
+++ Blythe.java
@@ -7,8 +7,8 @@ public class Blythe extends Npcs{
     }
 
     public Blythe(int x, int y){
-        super("Blythe", x, y, 0, 0, 61, 100, 
-        "assets/player/baseidlel.gif", "assets/player/baseidlel.gif", "assets/player/baseidler.gif", "assets/basewalkd.gif", "assets/basewalku.gif", "assets/basewalkl.gif", "assets/basewalkr.gif",
+        super("Blythe", x, y, 0, 0, 65, 100, 
+        "assets/npcs/blythe/blytheidlel.gif", "assets/npcs/blythe/blytheidlel.gif", "assets/npcs/blythe/blytheidler.gif", "assets/basewalkd.gif", "assets/basewalku.gif", "assets/basewalkl.gif", "assets/basewalkr.gif",
         false,
         new Dialogue("valentino's dialogue", "assets/dialogue/blytheDialogue"));
     }
