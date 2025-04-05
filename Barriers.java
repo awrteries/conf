88f8029077f8ee585034e37e3378a87113f6a3cb
awrteries <levic0rrpus@gmail.com>
2025-04-05 15:06:53 -0500

    SHAWTY GOT THE BASS I PULL UP TO HER PLACE BITCH I GET THE BAG N WE AINT TALKING FRITO LAYS SHE SAID I GET HER WETTER GET THE LETTER OFF MY WAIST

--- Barriers.java
+++ Barriers.java
@@ -16,16 +16,39 @@ public class Barriers extends Backgrounds{
     public void collision(Player p, Backgrounds b){
 
         // i dont even know.
-       if (p.getX()+p.getW()*2>getX()&&p.getX()<getX()+(getW()*2)){
-            if ((p.getY()+p.getH()*2)-20<getY()+getH()*2&&p.getY()+50>getY()+getH()){
-                p.setY(((getY()+getH()*2)-p.getH()*2)+20);
-                System.out.println("coll1" + this.getPic());
 
-            } else if (p.getY()+p.getH()*2>getY()&&p.getY()+p.getH()*2<getY()+getH()){
+       int pTop = p.getY();
+       int pBottom = p.getY()+p.getH()*2;
+       int pLeft = p.getX();
+       int pRight = p.getX()+p.getW()*2;
 
-                p.setY(getY()-p.getH()*2);
+       int top = getY();
+       int bottom = getY()+getH()*2;
+       int left =getX();
+       int right = getX()+getW()*2;
+       
+       
+      if (pLeft<right-20&&pRight>left+20){
+         if (pBottom<bottom+20 && pBottom>getY()+getH()){
+                p.setY((bottom-p.getH()*2)+20);
+            } else if (pBottom>top&&pBottom<getY()+getH()){
+                p.setY(top-p.getH()*2);
             }
-        }
+      }
+           
+       
+
+       if (pBottom>=top+5&&pBottom<=bottom-5){
+        System.out.println("kysw");
+            if (pLeft<right && pLeft>getX()+getW()){
+                p.setX(right);
+            } else if (pRight>left && pRight<getX()+getW()){
+                p.setX(left-p.getW()*2);
+            }
+       } 
+
+
+      
         
     }
 
