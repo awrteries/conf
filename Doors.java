ac03f9ce9bb529778f706ba11aa608c5f8968fc6
awrteries <levic0rrpus@gmail.com>
2025-04-07 13:39:57 -0500

    tweaked doors class so it doesnt mess up the run

--- Doors.java
+++ Doors.java
@@ -1,6 +1,7 @@
 public class Doors extends Props{
 
-    public Doors(){
-        super();
+    public Doors(String s, int x, int y ,int dx, int dy, int w, int h, boolean in){
+        super(s, x, y, dx, dy, w, h, in);
     }
+
 }
